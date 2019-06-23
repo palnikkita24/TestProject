362cbd14a857d20d71eeb0dc0c5557aa93a1989d
palnikkita <pal.nikkita@gmail.com>
2019-06-23 17:05:34 +0530

    update change

--- src/palnikkita_1_test.java
+++ src/palnikkita_1_test.java
@@ -1,1 +1,10 @@
-bin  palnikkita_1_test.java  src
+
+
+public class palnikkita_1_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("nikitapal");
+	}
+
+}
